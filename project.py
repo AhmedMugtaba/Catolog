f09cab6d6a0439cc5c2c10dc7c10deb4bb74227d
ahmedmugtaba <ahmedmugtaba88@gmail.com>
2017-11-28 14:08:24 -0500

    print commented out

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+F#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect, flash, \
     jsonify, url_for
@@ -110,7 +110,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(json.dumps(
             "Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        # print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
 
